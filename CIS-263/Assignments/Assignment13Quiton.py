5fab4110493487d9691be9bbb449ce113647da76
Alex Quiton <aquiton22@gmail.com>
2022-11-30 18:10:15 -0500

    Update Assignment13Quiton.py

--- CIS-263/Assignments/Assignment13Quiton.py
+++ CIS-263/Assignments/Assignment13Quiton.py
@@ -6,21 +6,25 @@ class Node:
         self.color = None
         #adj nodes
         self.neighbors = []
-        return
+        
 
 class Graph:
-    def __init(self):
+    def __init__(self):
         #list of all the nodes in the graph
         self.nodes = []
         #lsit of colors being used
         self.colors = []
-        return
+        
     
     #add node method
-    def insertNode(self,node,connectors): #add conectors here
+    def insertNode(self,newNode): #add conectors here
         #add node to graph
-        self.nodes.append(node)
-        #add adj/connectors
+        
+        self.nodes.append(newNode)
+        
+        
+    
+    def addConnections(self,node,connectors):
         if len(connectors) == 0:
             return
         else:
@@ -29,11 +33,17 @@ class Graph:
                     if(value == gnode.value):
                         node.neighbors.append(gnode)
                         break
-        return
+        
+
     
     #printgraph
     def displayGraph(self):
-        
+        for node in self.nodes:
+            print(node.value,end=" ")
+            neighbors = []
+            for neighbor in node.neighbors:
+                neighbors.append(neighbor.value)
+            print("connections: " + str(neighbors))
 
     #graphcolor method
     def colorGraph(self,starting_node):
@@ -43,3 +53,20 @@ class Graph:
         # check all colors to make sure they aren't the neighboring nodes color
         # }
         return
+
+
+newGraph = Graph()
+nodes = []
+connections = [["B","C"],["A","C"],["A","B"]]
+start = ord('a')
+for letter in range(97,104):
+    newNode = Node(chr(letter))
+    newGraph.insertNode(newNode)
+
+
+
+
+# newGraph.addConnections(node1, ["B","C"])
+# newGraph.addConnections(node2, ["A","C"])
+# newGraph.addConnections(node3, ["A","B"])
+newGraph.displayGraph()
